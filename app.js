6095c8f0fa7f5715af384da7143cc88016cb6178
Piotr Jaczewski <pp.jaczewski@gmail.com>
2019-09-03 22:13:27 +0200

    step2 test

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function shakeEventDidOccur () {
         main.classList.add('step1');
+        setTimeout(function(){ alert("step2 fired Up"); }, 5000);
     }
     
 });
