a69f50883121966c858e0919cec4c4a5daa0e66e
Piotr Jaczewski <pp.jaczewski@gmail.com>
2019-09-03 20:15:21 +0200

    first animations - text

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.addEventListener('shake', shakeEventDidOccur, false);
 
-    //function to call when shake occurs
     function shakeEventDidOccur () {
-        //put your own code here etc.
-        alert('shake!');
         main.classList.add('step1');
     }
     
