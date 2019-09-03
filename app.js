b1039010625944feaf13ffe11e36d3fed24ad4d7
Piotr Jaczewski <pp.jaczewski@gmail.com>
2019-09-03 19:08:43 +0200

    shake test

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('test');
+document.addEventListener('DOMContentLoaded', function () {
+    alert('działa')
+
+    const shakeEvent = new Shake();
+
+    const shakeHandler = () => {
+        alert('shake działa')
+    };
+
+    shakeEvent.start(shakeHandler);
+});
